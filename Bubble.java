0367efcbdb5053e191027fdd9ca382d5d58b0ff5
Homesh Dega <homeshdega06@gmail.com>
2025-05-26 11:42:21 +0530

    Create Bubble.java

---
+++ Bubble.java
@@ -0,0 +1,15 @@
+Bubble sort
+--------------------------------------------------
+class Bubble {
+    public static void bubbleSort(int arr[]) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[i]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+}
