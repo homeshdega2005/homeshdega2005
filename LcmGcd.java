a16b75f00346c6d65a4cbf873880b486eb944979
Homesh Dega <homeshdega06@gmail.com>
2025-05-28 11:41:24 +0530

    Create LcmGcd.java

---
+++ LcmGcd.java
@@ -0,0 +1,15 @@
+Lcm & Gcd
+--------------------------------------
+class LcmGcd{
+    public static int[] lcmAndGcd(int a, int b) {
+        int prod=a*b;
+        while(b!=0){
+            int temp=a%b;
+            a=b;
+            b=temp;
+        }
+        int gcd=a;
+        int lcm=prod/gcd;
+        return new int[] {lcm,gcd};
+    }
+}
