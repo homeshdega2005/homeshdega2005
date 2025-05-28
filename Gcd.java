7a49ada57d6c41be3201a91b1395b2e03ad9e7c3
Homesh Dega <homeshdega06@gmail.com>
2025-05-28 11:00:39 +0530

    Create Gcd.java

---
+++ Gcd.java
@@ -0,0 +1,12 @@
+GCD
+---------------------------
+class Gcd {
+    public static int gcd(int a, int b) {
+            while(b!=0){
+            int temp=a%b;
+            a=b;
+            b=temp;
+        }
+        return a;
+    }
+}
